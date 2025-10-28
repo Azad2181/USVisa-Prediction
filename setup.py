875a6ee423c267c558b583bec14d89e15bde5df4
Azad2181 <abulkalamazad2181@gmail.com>
2025-10-28 12:19:02 +0600

    added requirements and template

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+setup(
+    name="US_visa",
+    version="0.0.0",
+    author="azad",
+    author_email="abulkalamazad2181@gmail.com",
+    packages=find_packages()
+)
